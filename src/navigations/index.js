ee443784ee0a3d342f559d667ceaf36fd3be321c
plotrekhq <plotrekhqpl@gmail.com>
2021-07-03 20:10:31 +0200

    Authentication flow

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,18 +1,52 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './DrawerNavigator';
 import AuthNavigator from './AuthNavigator';
 import {GlobalContext} from '../context/Provider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ActivityIndicator} from 'react-native';
+
 const AppNavContainer = () => {
   const {
     authState: {isLoggedIn},
   } = useContext(GlobalContext);
-  console.log('isLoggedIn :>>', isLoggedIn);
+
+  const [isAutenticated, setIsAutenticated] = useState(false);
+  const [authLoaded, setAuthLoaded] = useState(false);
+
+  const getUser = async () => {
+    try {
+      const user = await AsyncStorage.getItem('token');
+      if (user) {
+        setAuthLoaded(true);
+        setIsAutenticated(true);
+      } else {
+        setAuthLoaded(true);
+        setIsAutenticated(false);
+      }
+    } catch (error) {}
+  };
+
+  console.log('isLoggedIn :>>', isAutenticated);
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
-    <NavigationContainer>
-      {isLoggedIn ? <DrawerNavigator /> : <AuthNavigator />}
-    </NavigationContainer>
+    <>
+      {authLoaded ? (
+        <NavigationContainer>
+          {isLoggedIn || isAutenticated ? (
+            <DrawerNavigator />
+          ) : (
+            <AuthNavigator />
+          )}
+        </NavigationContainer>
+      ) : (
+        <ActivityIndicator/>
+      )}
+    </>
   );
 };
 
